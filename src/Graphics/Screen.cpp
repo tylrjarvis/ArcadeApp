fafd8cd621de8a5297dd7d7b3ef7c40000e6c0b0
Tyler Jarvis <tylrjarvis@yahoo.com>
2025-04-22 21:03:41 -0500

    add DrawLine

--- src/Graphics/Screen.cpp
+++ src/Graphics/Screen.cpp
@@ -3,7 +3,7 @@
 #include "Line2D.h"
 #include <SDL3/SDL.h>
 #include <cassert>
-
+#include <cmath>
 Screen::Screen() : mWidth(0), mHeight(0), mWindow(nullptr), windowSurface(nullptr)
 {
     
@@ -94,5 +94,64 @@ void Screen::Draw(const Vec2D& point, const Color& color)
     if(mWindow)
     {
         mBackBuffer.SetPixel(color, point.GetX(), point.GetY());
+    }
+}
+
+void Screen::Draw(const Line2D& line, const Color& color)
+{
+    assert(mWindow);
+    if(mWindow)
+    {
+
+        int x0 = roundf(line.GetP0().GetX());
+        int y0 = roundf(line.GetP0().GetY());
+        int x1 = roundf(line.GetP1().GetX());
+        int y1 = roundf(line.GetP1().GetY());
+
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+
+        signed const char ix((dx > 0) - (dx < 0));
+        signed const char iy((dy > 0) - (dy < 0));
+
+        dx = abs(dx) * 2;
+        dy = abs(dy) * 2;
+
+        Draw(x0, y0, color);
+
+        if(dx >= dy)
+        {
+            int d = dy - dx/2;
+
+            while(x0 != x1)
+            {
+                if(d >= 0)
+                {
+                    d -= dx;
+                    y0 += iy;
+                }
+                d += dy;
+                x0 += ix;
+
+                Draw(x0, y0, color);
+            }
+        }
+        else
+        {
+            int d = dx - dy/2;
+
+            while(y0 != y1)
+            {
+                if(d >= 0)
+                {
+                    d -= dy;
+                    x0 += ix;
+                }
+                d += dx;
+                y0 += iy;
+
+                Draw(x0, y0, color);
+            }
+        }
     }
 }
