11549bc1f3bbd97551fb00489854cbb1efaafb52
Tyler Jarvis <tylrjarvis@yahoo.com>
2025-06-24 19:49:44 -0500

    corrected spelling mistake in the Draw function for the scene

--- src/Scenes/Scene.h
+++ src/Scenes/Scene.h
@@ -4,13 +4,15 @@
 #include <string>
 #include <stdint.h>
 
+class Screen;
+
 class Scene
 {
     public:
         virtual ~Scene() {};
         virtual void Init() = 0;
         virtual void Update(uint64_t dt) = 0;
-        virtual void Draw(Scene& theScreen) = 0;
+        virtual void Draw(Screen& theScreen) = 0;
         virtual const std::string& GetSceneName() const = 0;
 };
 
