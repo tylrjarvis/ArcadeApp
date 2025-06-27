d0cea968bf137b73986c7a0ef6fa5bc2f2c3e831
Tyler Jarvis <tylrjarvis@yahoo.com>
2025-06-26 20:39:17 -0500

    Add InputController header file

---
+++ src/Input/InputController.h
@@ -0,0 +1,21 @@
+#ifndef INPUT_INPUTCONTROLLER_H_
+#define INPUT_INPUTCONTROLLER_H_
+
+#include "InputAction.h"
+
+struct GameController;
+
+class InputController
+{
+    public:
+        InputController();
+        void Init(InputAction quitAction);
+        void Update(uint32_t dt);
+        void SetGameController(GameController* controller);
+        
+    private:
+        InputAction mQuit;
+        GameController* mnoptrCurrentController;
+};
+
+#endif /* INPUT_INPUTCONTROLLER_H_ */
