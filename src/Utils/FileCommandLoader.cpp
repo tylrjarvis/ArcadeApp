abb0bbeb3caa649673ae4193a3366db4755e4966
Tyler Jarvis <tylrjarvis@yahoo.com>
2025-06-29 09:26:03 -0500

    Created CPP file for FileCommandLoader with stubbed functions

---
+++ src/Utils/FileCommandLoader.cpp
@@ -0,0 +1,41 @@
+#include "FileCommandLoader.h"
+#include <fstream>
+#include <iostream>
+#include "Color.h"
+#include "Vec2D.h"
+
+void FileCommandLoader::AddCommand(const Command& command)
+{
+
+}
+
+void FileCommandLoader::LoadFile(const std::string& filePath)
+{
+
+}
+
+
+Color FileCommandLoader::ReadColor(const ParseFuncParams& params)
+{
+
+}
+
+Vec2D FileCommandLoader::ReadSize(const ParseFuncParams& params)
+{
+
+}
+
+int FileCommandLoader::ReadInt(const ParseFuncParams& params)
+{
+
+}
+
+std::string FileCommandLoader::ReadString(const ParseFuncParams& params)
+{
+
+}
+
+char FileCommandLoader::ReadChar(const ParseFuncParams& params)
+{
+
+}
