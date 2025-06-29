45865fda4a35f2184fa802d9fce69b9abf8d3ad4
Tyler Jarvis <tylrjarvis@yahoo.com>
2025-06-29 09:43:32 -0500

    ReadColor function defined

--- src/Utils/FileCommandLoader.cpp
+++ src/Utils/FileCommandLoader.cpp
@@ -6,7 +6,7 @@
 
 void FileCommandLoader::AddCommand(const Command& command)
 {
-
+    mCommands.push_back(command);
 }
 
 void FileCommandLoader::LoadFile(const std::string& filePath)
@@ -17,7 +17,20 @@ void FileCommandLoader::LoadFile(const std::string& filePath)
 
 Color FileCommandLoader::ReadColor(const ParseFuncParams& params)
 {
+    size_t nextSpacePos = params.line.find_first_of(" ", params.delimitPos+1);
+    int r = std::stoi(params.line.substr(params.delimitPos, (nextSpacePos - params.delimitPos)));
+    
+    size_t lastSpacePos = nextSpacePos;
+    nextSpacePos = params.line.find_first_of(" ", lastSpacePos + 1);
+    int g = std::stoi(params.line.substr(lastSpacePos+1, nextSpacePos - lastSpacePos));
 
+    lastSpacePos = nextSpacePos;
+    nextSpacePos = params.line.find_first_of(" ", lastSpacePos + 1);
+    int b = std::stoi(params.line.substr(lastSpacePos+1, nextSpacePos - lastSpacePos));
+
+    int a = std::stoi(params.line.substr(nextSpacePos+1));
+
+    return Color(r, g, b, a);
 }
 
 Vec2D FileCommandLoader::ReadSize(const ParseFuncParams& params)
